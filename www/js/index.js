98e317406b1158de7929156950e1d2e64a5605eb
Dhruv Chand <dhruvchand@live.com>
2015-01-17 03:20:23 +0530

    Checks for first run

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,6 @@
 
+var AppSettings = new Object();
+
 var app = {
     SOME_CONSTANTS : false,  // some constant
 
@@ -20,8 +22,21 @@ var app = {
 
     onDeviceReady: function() {
         console.log("device ready, start making you custom calls!");
-
+		
+		checkFirstRun();
         //Moar code
 
     }
 };
+
+function checkFirstRun()
+{
+	if(typeof( window.localStorage.WoSApp) == "undefined")
+	{
+		//go to config page
+		window.location= "#firstrun-page";
+		//populate details into settings var
+		
+		window.localStorage.setItem("WoSApp",AppSettings);
+	}
+}
